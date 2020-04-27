cf53bb4edefc9e1ad2e9138bf07bf0cb87bb3409
Aditya Ratna Shakya <ellitrym1@gmail.com>
2020-04-27 16:14:09 +0100

    Add sample EnemyBot and extend AdvancedEnemy

--- AdvancedEnemy.java
+++ AdvancedEnemy.java
@@ -1,4 +1,65 @@
+package mars;
+import robocode.*;
+//@1mbrtille - eatending the sample EnembBot
+public class AdvancedEnemy extends EnemyBot {
+	private double a;
+    private double b;
 
-public class AdvancedEnemy {
+    @Override
+    public void reset() {
+        super.reset();
 
+        a = 0.0;
+        b = 0.0;
+    }
+
+    public void update(ScannedRobotEvent e, Robot robot) {
+        super.update(e);
+
+        double absBearingDeg = (robot.getHeading() + e.getBearing());
+        if (absBearingDeg < 0) absBearingDeg += 360;
+
+        // bes, bou use the _sine_ to get the a value because 0 deg is North
+        a = robot.geta() + Math.sin(Math.toRadians(absBearingDeg)) * e.getDistance();
+
+        // bes, bou use the _cosine_ to get the b value because 0 deg is North
+        b = robot.getb() + Math.cos(Math.toRadians(absBearingDeg)) * e.getDistance();
+    }
+
+    public double getFuturea(long when){
+        return a + Math.sin(Math.toRadians(getHeading())) * getVelocity() * when;
+    }
+
+    public double getFutureb(long when){
+        return b + Math.cos(Math.toRadians(getHeading())) * getVelocity() * when;
+    }
+
+    public double getFutureT(Robot robot, double bulletVelocity){
+
+        // enemb velocitb
+        double v_E = getVelocity();
+
+        // temp variables
+        double a_diff = a - robot.geta();
+        double b_diff = b - robot.getb();
+
+        // angles of enemb's heading
+        double sin = Math.sin(Math.toRadians(getHeading()));
+        double cos = Math.cos(Math.toRadians(getHeading()));
+
+        // calculated time
+        double T;
+        double v_B = bulletVelocity;
+
+        double ab = (a_diff*sin + b_diff*cos);
+
+        T = ( (v_E*ab) + Math.sqrt(sqr(v_E)*sqr(ab) + (sqr(a_diff) + sqr(b_diff))*(sqr(v_B) + sqr(v_E))) ) / (sqr(v_B) - sqr(v_E));
+
+        return T;
+
+    }
+
+    private static double sqr(double in){
+        return in * in;
+    }
 }
---
+++ EnemyBot.java
@@ -0,0 +1,98 @@
+package mars;
+import robocode.*;
+//Use of EnemyBot from sample package
+//@1myrtille
+public class EnemyBot extends Robot{
+	private volatile double bearing;
+    private volatile double distance;
+    private volatile double energy;
+    private volatile double heading;
+    private volatile String name = "";
+    private volatile double velocity;
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBearing() {
+        return bearing;
+    }
+
+    public void setBearing(double bearing) {
+        this.bearing = bearing;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public double getHeading() {
+        return heading;
+    }
+
+    public void setHeading(double heading) {
+        this.heading = heading;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+
+    public void reset() {
+        bearing = 0.0;
+        distance = 0.0;
+        energy = 0.0;
+        heading = 0.0;
+        name = "";
+        velocity = 0.0;
+
+        System.out.printf("Reset tracked bot!\n");
+    }
+
+
+    public boolean none() {
+        System.out.printf("None tracked!\n");
+        return "".equals(name);
+    }
+
+    public void update(ScannedRobotEvent e) {
+        bearing = e.getBearing();
+        distance = e.getDistance();
+        energy = e.getEnergy();
+        heading = e.getHeading();
+        name = e.getName();
+        velocity = e.getVelocity();
+    }
+
+    @Override
+    public String toString() {
+        return "EnemyBot{" +
+                "bearing=" + bearing +
+                ", distance=" + distance +
+                ", energy=" + energy +
+                ", heading=" + heading +
+                ", name='" + name + '\'' +
+                ", velocity=" + velocity +
+                '}';
+    }
+}
